f86899477852fe65cedea9b5ba4a3b224b408c5a
msadeghi <45124652+msadeghim@users.noreply.github.com>
2020-07-13 22:27:44 +0430

    Create Matching Anything But a Newline.py

---
+++ introduction/Matching Anything But a Newline.py
@@ -0,0 +1,12 @@
+
+regex_pattern = r"^.{3}\..{3}\..{3}\..{3}$"    # Do not delete 'r'.
+#regex_pattern = r"^...\....\....\....$"
+
+import re
+import sys
+
+test_string = input()
+
+match = re.match(regex_pattern, test_string) is not None
+
+print(str(match).lower())

e44f6dfbd25065b559961747fc7c9306e63df9b5
msadeghi <45124652+msadeghim@users.noreply.github.com>
2020-07-14 09:40:36 +0430

    Create UK and US: Part 2.py

---
+++ Applications/UK and US: Part 2.py
@@ -0,0 +1,12 @@
+import re
+x = int(input())
+lines = [input() for i in range(x)]
+text = ' '.join(lines)
+ 
+for x in range(int(input())):
+    ans = 0
+    a = input()
+    b = a.replace('our', 'or')
+    ans += len(re.findall(r'\b'+a+r'\b', text))
+    ans += len(re.findall(r'\b'+b+r'\b', text))
+    print(ans)
